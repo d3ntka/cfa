d127b5d3dbdbc0f552e106c8e5bcc5fb5ce2f9be
Koniec <d3ntka@gmail.com>
2022-04-05 17:28:09 +0200

    Prepare: files

---
+++ wp-content/themes/cfa/components/influ/influ-vars.php
@@ -0,0 +1,21 @@
+<?php
+
+// ------------Meta---------------
+
+$nick = get_the_title();
+$name = get_field( 'name' ); //Imię i nazwisko
+$desc = get_field( 'description' ); //Opis
+$img = the_post_thumbnail_url();
+
+
+// ---------------Socials---------------
+
+$twitter_url = get_field( 'twitter_url' );
+$twitter_count = get_field( 'twitter_count' );
+$twitch_url = get_field( 'twitch_url' );
+$twitch_count = get_field( 'twitch_count' );
+$facebook_url = get_field( 'facebook_url' );
+$facebook_count = get_field( 'facebook_count' );
+
+
+?>
