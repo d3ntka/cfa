a8bb22a0259ac6c882c5e65a4a3d1d99d39f7b94
Koniec <d3ntka@gmail.com>
2022-06-15 15:55:21 +0200

    fix: text_image img size

--- wp-content/themes/cfa/functions.php
+++ wp-content/themes/cfa/functions.php
@@ -60,7 +60,7 @@ if ( ! function_exists( 'cfa_setup_theme' ) ) :
 			)
 		);
 
-		add_image_size('text_image', 552, '', true); // Text-image Thumbnail
+		add_image_size('text_image', 670); // Text-image Thumbnail
 
 		// Add support for Block Styles.
 		add_theme_support( 'wp-block-styles' );
